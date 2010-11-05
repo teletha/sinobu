d48906165bce4ecee25e35633a0f5ff10be2f4bb
Teletha <Teletha@localhost>
2010-11-06 07:46:30 +0900

    Add tests for JDK.

--- src/test/java/ezbean/jdk/PropertyDescriptorIsNotPatheticTest.java
+++ src/test/java/ezbean/jdk/PropertyDescriptorIsNotPatheticTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ezbean.model;
+package ezbean.jdk;
 
 import static junit.framework.Assert.*;
 
