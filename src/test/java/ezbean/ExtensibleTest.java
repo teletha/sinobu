29752145e27b8005cfe641679a5cd316a13b6ee1
Teletha <Teletha.T@gmail.com>
2011-01-27 18:37:20 +0900

    Rewrite license comment.

--- src/test/java/ezbean/ExtensibleTest.java
+++ src/test/java/ezbean/ExtensibleTest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2010 Nameless Production Committee.
+/**
+ * Copyright (C) 2011 Nameless Production Committee.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ezbean;
 
 import static org.junit.Assert.*;
