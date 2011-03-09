92b69fa751f53b918c674b8ecb1a5009c2e883a8
Teletha <Teletha@localhost>
2011-03-09 21:22:51 +0900

    Clean unused test files.

--- src/test/java/ezunit/Filer.java
+++ src/test/java/ezunit/Filer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ezunit.io;
+package ezunit;
 
 import static java.nio.file.FileVisitResult.*;
 import static java.nio.file.StandardCopyOption.*;
