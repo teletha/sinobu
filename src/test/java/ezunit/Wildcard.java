bdc707da2df9fac308788bfc3487e858772c4def
Teletha <Teletha.T@gmail.com>
2011-03-10 17:07:23 +0900

    Remove unused mime-type detector.

--- src/test/java/ezunit/Wildcard.java
+++ src/test/java/ezunit/Wildcard.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ezbean;
+package ezunit;
 
 import java.util.ArrayList;
 import java.util.Arrays;
