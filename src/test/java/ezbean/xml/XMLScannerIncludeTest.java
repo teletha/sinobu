958915c74fd77acd3dd31dce0b46f9ebf840deb2
Teletha <Teletha.T@gmail.com>
2011-04-15 16:12:34 +0900

    XMLScanner#include can accept event filter.

--- src/test/java/ezbean/xml/XMLScannerIncludeTest.java
+++ src/test/java/ezbean/xml/XMLScannerIncludeTest.java
@@ -18,6 +18,8 @@ package ezbean.xml;
 import static ezunit.Ezunit.*;
 
 import org.junit.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 
 /**
  * @version 2011/04/14 10:36:03
@@ -38,6 +40,22 @@ public class XMLScannerIncludeTest {
         };
 
         assertXMLIdentical("include/pathExpected.xml", "include/path.xml", scanner);
+    }
+
+    @Test
+    public void pathWithFilter() throws Exception {
+        XMLScanner scanner = new XMLScanner() {
+
+            @SuppressWarnings("unused")
+            @Rule(match = "root")
+            public void in() {
+                start("root");
+                include(locate("include/pathIncluded.xml"), new StripChildElement());
+                end();
+            }
+        };
+
+        assertXMLIdentical("include/pathWithFilterExpected.xml", "include/path.xml", scanner);
     }
 
     @Test
@@ -92,5 +110,33 @@ public class XMLScannerIncludeTest {
         };
 
         assertXMLIdentical("include/bitsProceedExpected.xml", "include/bits.xml", scanner);
+    }
+
+    /**
+     * @version 2011/04/15 13:45:43
+     */
+    private static class StripChildElement extends XMLScanner {
+
+        /**
+         * @see org.xml.sax.helpers.XMLFilterImpl#startElement(java.lang.String, java.lang.String,
+         *      java.lang.String, org.xml.sax.Attributes)
+         */
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+            if (!localName.equals("child")) {
+                super.startElement(uri, localName, qName, atts);
+            }
+        }
+
+        /**
+         * @see org.xml.sax.helpers.XMLFilterImpl#endElement(java.lang.String, java.lang.String,
+         *      java.lang.String)
+         */
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (!localName.equals("child")) {
+                super.endElement(uri, localName, qName);
+            }
+        }
     }
 }
