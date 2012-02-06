f9cdf08e223bc8dd45af4e67e873eb24a2cff50d
Teletha <Teletha.T@gmail.com>
2012-02-06 17:03:14 +0900

    Clean up Element code. Element recognizes adjacent sibling combinator.

--- src/test/java/kiss/xml/Element.java
+++ src/test/java/kiss/xml/Element.java
@@ -11,14 +11,21 @@ package kiss.xml;
 
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
@@ -29,7 +36,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 /**
- * @version 2012/02/05 17:07:25
+ * @version 2012/02/06 16:24:40
  */
 public class Element implements Iterable<Element> {
 
@@ -37,19 +44,25 @@ public class Element implements Iterable<Element> {
      * Original pattern.
      * 
      * <pre>
-     * ([>+~ ])?(\w+|\*)?(#(\w+))?((\.\w+)*)(\[(\w+)\s?([=~^$*|])?=\s?["]([^"]*)["]\])?(:([\w-]+)\((.*)\))?
+     * ([>+~ ])?(\w+|\*)?(#(\w+))?((\.\w+)*)(\[\s?(\w+)(\s?([=~^$*|])?=\s?["]([^"]*)["])?\s?\])?(:([\w-]+)\((.*)\))?
      * </pre>
      */
-    private static final Pattern Selector = Pattern.compile("([>+~ ])?(\\w+|\\*)?(#(\\w+))?((\\.\\w+)*)(\\[(\\w+)\\s?([=~^$*|])?=\\s?[\"]([^\"]*)[\"]\\])?(:([\\w-]+)\\((.*)\\))?");
+    private static final Pattern SELECTOR = Pattern.compile("([>+~ ])?(\\w+|\\*)?(#(\\w+))?((\\.\\w+)*)(\\[\\s?(\\w+)(\\s?([=~^$*|])?=\\s?[\"]([^\"]*)[\"])?\\s?\\])?(:([\\w-]+)\\((.*)\\))?");
 
-    private static DocumentBuilder dom;
+    /** The cache for compiled selectors. */
+    private static final Map<String, XPathExpression> selectors = new ConcurrentHashMap();
 
-    private static XPathFactory XPath;
+    /** The document builder. */
+    private static DocumentBuilder DOM;
 
+    /** The xpath evaluator. */
+    private static XPath XPATH;
+
+    // initialization
     static {
         try {
-            dom = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            XPath = XPathFactory.newInstance();
+            DOM = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            XPATH = XPathFactory.newInstance().newXPath();
         } catch (Exception e) {
             throw I.quiet(e);
         }
@@ -59,108 +72,70 @@ public class Element implements Iterable<Element> {
         return new Element(xml);
     }
 
-    private List<Node> nodes = new ArrayList();
+    /** The current node set. */
+    private final Set<Node> nodes;
 
+    /**
+     * <p>
+     * From xml text.
+     * </p>
+     * 
+     * @param xml
+     */
     private Element(String xml) {
         try {
-            nodes.add(dom.parse(new InputSource(new StringReader(xml))).getDocumentElement());
+            nodes = Collections.singleton(DOM.parse(new InputSource(new StringReader(xml))).getFirstChild());
         } catch (Exception e) {
             throw I.quiet(e);
         }
     }
 
-    private Element(List<Node> nodes) {
+    /**
+     * <p>
+     * From node set.
+     * </p>
+     * 
+     * @param nodes
+     */
+    private Element(Set<Node> nodes) {
         this.nodes = nodes;
     }
 
+    /**
+     * <p>
+     * Get the descendants of each element in the current set of matched elements, filtered by a css
+     * selector.
+     * </p>
+     * 
+     * @param selector A string containing a css selector expression to match elements against.
+     * @return
+     */
     public Element find(String selector) {
-        selector = selector.replaceAll("\\s+", " ").replaceAll(" ([>+~]) ", "$1");
-
-        StringBuilder xpath = new StringBuilder();
-        Matcher matcher = Selector.matcher(selector);
-
-        while (matcher.find()) {
-            // connector
-            String suffix = null;
-            String match = matcher.group(1);
-
-            if (match == null) {
-                if (matcher.start() == 0) {
-                    xpath.append("descendant::");
-                } else {
-                    break;
-                }
-            } else {
-                switch (match.charAt(0)) {
-                case '>':
-                    xpath.append('/');
-                    break;
-
-                case '+':
-                    xpath.append("/following-sibling::*[1][self::");
-                    suffix = "]";
-                    break;
-
-                case '~':
-                    break;
-
-                case ' ':
-                    xpath.append("//");
-                    break;
-                }
-            }
-
-            // type
-            match = matcher.group(2);
-
-            if (match == null) {
-                xpath.append("*");
-            } else {
-                xpath.append(match);
-            }
-
-            if (suffix != null) {
-                xpath.append(suffix);
-            }
-
-            // #id
-            match = matcher.group(4);
-
-            if (match != null) {
-                xpath.append("[@id='" + match + "']");
-            }
-
-            // .class
-            match = matcher.group(5);
-
-            if (match != null && match.length() != 0) {
-                for (String className : match.substring(1).split("\\.")) {
-                    xpath.append("[contains(concat(' ',normalize-space(@class),' '),' " + className + " ')]");
-                }
-            }
-
-        }
-
-        System.out.println(xpath);
-        List<Node> nodes = new ArrayList();
+        XPathExpression xpath = compile(selector);
+        Set<Node> result = new HashSet();
 
         try {
-            for (Node node : this.nodes) {
-                NodeList list = (NodeList) XPath.newXPath().evaluate(xpath.toString(), node, XPathConstants.NODESET);
+            for (Node node : nodes) {
+                NodeList list = (NodeList) xpath.evaluate(node, XPathConstants.NODESET);
 
                 for (int i = 0; i < list.getLength(); i++) {
-                    nodes.add(list.item(i));
+                    result.add(list.item(i));
                 }
             }
 
-            System.out.println(nodes.size());
-
-            return new Element(nodes);
+            return new Element(result);
         } catch (XPathExpressionException e) {
             throw I.quiet(e);
         }
     }
 
+    /**
+     * <p>
+     * Return size of the current node set.
+     * </p>
+     * 
+     * @return
+     */
     public int size() {
         return nodes.size();
     }
@@ -170,6 +145,205 @@ public class Element implements Iterable<Element> {
      */
     @Override
     public Iterator<Element> iterator() {
-        return null;
+        List<Element> elements = new ArrayList();
+
+        for (Node node : nodes) {
+            elements.add(new Element(Collections.singleton(node)));
+        }
+        return elements.iterator();
+    }
+
+    /**
+     * <p>
+     * Compile and cache the specified selector.
+     * </p>
+     * 
+     * @param selector A css selector.
+     * @return A compiled xpath expression.
+     */
+    private static XPathExpression compile(String selector) {
+        // check cache
+        XPathExpression compiled = selectors.get(selector);
+
+        if (compiled == null) {
+            // normalize space
+            String pattern = selector.replaceAll("\\s+", " ").replaceAll(" ([>+~]) ", "$1");
+
+            // compile css selector to xpath
+            StringBuilder xpath = new StringBuilder();
+            Matcher matcher = SELECTOR.matcher(pattern);
+
+            while (matcher.find()) {
+                // =================================================
+                // Combinators
+                // =================================================
+                String suffix = null;
+                String match = matcher.group(1);
+
+                if (match == null) {
+                    // no combinator
+                    if (matcher.start() == 0) {
+                        // first selector
+                        xpath.append("descendant::");
+                    } else {
+                        break; // finish parsing
+                    }
+                } else {
+                    switch (match.charAt(0)) {
+                    case '>': // Child combinator
+                        xpath.append('/');
+                        break;
+
+                    case ' ': // Descendant combinator
+                        xpath.append("//");
+                        break;
+
+                    case '~': // General sibling combinator
+                        xpath.append("/following-sibling::");
+                        break;
+
+                    case '+': // Adjacent sibling combinator
+                        xpath.append("/following-sibling::*[1][self::");
+                        suffix = "]";
+                        break;
+                    }
+                }
+
+                // =================================================
+                // Type (Universal) Selector
+                // =================================================
+                match = matcher.group(2);
+
+                if (match == null) {
+                    xpath.append("*");
+                } else {
+                    xpath.append(match);
+                }
+
+                if (suffix != null) {
+                    xpath.append(suffix);
+                }
+
+                // =================================================
+                // ID Selector
+                // =================================================
+                match = matcher.group(4);
+
+                if (match != null) {
+                    xpath.append("[@id='" + match + "']");
+                }
+
+                // =================================================
+                // Class Selector
+                // =================================================
+                match = matcher.group(5);
+
+                if (match != null && match.length() != 0) {
+                    for (String className : match.substring(1).split("\\.")) {
+                        xpath.append("[contains(concat(' ',normalize-space(@class),' '),' " + className + " ')]");
+                    }
+                }
+
+                // =================================================
+                // Attribute Selector
+                // =================================================
+                match = matcher.group(8);
+
+                if (match != null) {
+                    String value = matcher.group(11);
+
+                    if (value == null) {
+                        // [att]
+                        //
+                        // Represents an element with the att attribute, whatever the value
+                        // of the attribute.
+                        xpath.append("[@").append(match).append("]");
+                    } else {
+                        String type = matcher.group(10);
+
+                        if (type == null) {
+                            // [att=val]
+                            //
+                            // Represents an element with the att attribute whose value
+                            // is exactly "val".
+                            xpath.append("[@").append(match).append("='").append(value).append("']");
+                        } else {
+                            switch (type.charAt(0)) {
+                            case '~':
+                                // [att~=val]
+                                //
+                                // Represents an element with the att attribute whose value is a
+                                // whitespace-separated list of words, one of which is exactly
+                                // "val". If "val" contains whitespace, it will never represent
+                                // anything (since the words are separated by spaces). Also if "val"
+                                // is the empty string, it will never represent anything.
+                                xpath.append("[contains(concat(' ',@")
+                                        .append(match)
+                                        .append(",' '),' ")
+                                        .append(value)
+                                        .append(" ')]");
+                                break;
+
+                            case '*':
+                                // [att*=val]
+                                //
+                                // Represents an element with the att attribute whose value contains
+                                // at least one instance of the substring "val". If "val" is the
+                                // empty string then the selector does not represent anything.
+                                xpath.append("[contains(@").append(match).append(",'").append(value).append("')]");
+                                break;
+
+                            case '^':
+                                // [att^=val]
+                                //
+                                // Represents an element with the att attribute whose value begins
+                                // with the prefix "val". If "val" is the empty string then the
+                                // selector does not represent anything.
+                                xpath.append("[starts-with(@").append(match).append(",'").append(value).append("')]");
+                                break;
+
+                            case '$':
+                                // [att$=val]
+                                //
+                                // Represents an element with the att attribute whose value ends
+                                // with the suffix "val". If "val" is the empty string then the
+                                // selector does not represent anything.
+                                xpath.append("[substring(@")
+                                        .append(match)
+                                        .append(", string-length(@")
+                                        .append(match)
+                                        .append(") - string-length('")
+                                        .append(value)
+                                        .append("') + 1) = '")
+                                        .append(value)
+                                        .append("']");
+                                break;
+
+                            case '|':
+                                // [att|=val]
+                                //
+                                // Represents an element with the att attribute, its value either
+                                // being exactly "val" or beginning with "val" immediately followed
+                                // by "-" (U+002D).
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            try {
+                // compile actually
+                compiled = XPATH.compile(xpath.toString());
+
+                // cache it
+                selectors.put(selector, compiled);
+            } catch (XPathExpressionException e) {
+                throw I.quiet(e);
+            }
+        }
+
+        // API definition
+        return compiled;
     }
 }
--- src/test/java/kiss/xml/ElementFindTest.java
+++ src/test/java/kiss/xml/ElementFindTest.java
@@ -11,65 +11,154 @@ package kiss.xml;
 
 import static kiss.xml.Element.*;
 
+import java.io.StringReader;
+
+import kiss.I;
+
 import org.junit.Test;
+import org.xml.sax.InputSource;
 
 /**
  * @version 2012/02/05 17:08:12
  */
 public class ElementFindTest {
 
-    /** The test document. */
-    private static final String document = ""
-            + "<root>"
-            + "     <person class='fire sisters' id='tukihi'>"
-            + "         <age>14</age>"
-            + "         <person class='brother'>"
-            + "             <name>Araragi Koyomi</name>"
-            + "         </person>"
-            + "         <friend>"
-            + "             <person class='friend'>"
-            + "                 <name>Hanekawa Tubasa</name>"
-            + "             </person>"
-            + "         </friend>"
-            + "     </person>"
-            + "     <person class='fire sisters' id='karen'>"
-            + "         <age>14</age>"
-            + "         <person class='brother'>"
-            + "             <name>Araragi Koyomi</name>"
-            + "         </person>"
-            + "     </person>"
-            + "     <person class='snake' id='nadeko'>"
-            + "         <age>14</age>"
-            + "     </person>"
-            + "</root>";
-
     @Test
     public void type() throws Exception {
-        assert $(document).find("person").size() == 6;
+        String xml = "<m><E/><E/><e><E/></e></m>";
+
+        assert $(xml).find("E").size() == 3;
     }
 
     @Test
     public void clazz() throws Exception {
-        assert $(document).find(".fire").size() == 2;
+        String xml = "<m><e class='C'/><e class='none'/></m>";
+
+        assert $(xml).find(".C").size() == 1;
+    }
+
+    @Test
+    public void clazzWithMultipleValue() throws Exception {
+        String xml = "<m><e class='A B C'/><e class='AA BB CC'/></m>";
+
+        assert $(xml).find(".A").size() == 1;
+        assert $(xml).find(".B").size() == 1;
+        assert $(xml).find(".C").size() == 1;
     }
 
     @Test
     public void clazzMultiple() throws Exception {
-        assert $(document).find(".sisters.fire").size() == 2;
+        String xml = "<m><e class='A B C'/><e class='AA BB CC'/></m>";
+
+        assert $(xml).find(".A.B").size() == 1;
+        assert $(xml).find(".B.C").size() == 1;
+        assert $(xml).find(".C.A").size() == 1;
     }
 
     @Test
     public void id() throws Exception {
-        assert $(document).find("#nadeko").size() == 1;
+        String xml = "<m><e id='A'/><e id='AA'/></m>";
+
+        assert $(xml).find("#A").size() == 1;
+        assert $(xml).find("#AA").size() == 1;
+    }
+
+    @Test
+    public void attribute() throws Exception {
+        String xml = "<m><e A='A' B='B'/><e A='B' B='A'/></m>";
+
+        assert $(xml).find("[A]").size() == 2;
+        assert $(xml).find("[B]").size() == 2;
+    }
+
+    @Test
+    public void attributeValue() throws Exception {
+        String xml = "<m><e A='A'/></m>";
+
+        // variants for white space
+        assert $(xml).find("[A=\"A\"]").size() == 1;
+        assert $(xml).find("[A = \"A\"]").size() == 1;
+        assert $(xml).find("[A       =    \"A\"]").size() == 1;
+        assert $(xml).find("[ A = \"A\" ]").size() == 1;
+    }
+
+    @Test
+    public void attributeConatainValue() throws Exception {
+        String xml = "<m><e A='A B C'/><e A='AA BB CC'/></m>";
+
+        assert $(xml).find("[A ~= \"A\"]").size() == 1;
+        assert $(xml).find("[A ~= \"B\"]").size() == 1;
+        assert $(xml).find("[A ~= \"C\"]").size() == 1;
+    }
+
+    @Test
+    public void attributeConatainText() throws Exception {
+        String xml = "<m><e A='A B C'/><e A='AB'/></m>";
+
+        assert $(xml).find("[A *= \"A\"]").size() == 2;
+        assert $(xml).find("[A *= \"B\"]").size() == 2;
+        assert $(xml).find("[A *= \"C\"]").size() == 1;
+    }
+
+    @Test
+    public void attributeStartWith() throws Exception {
+        String xml = "<m><e A='A B C'/><e A='AA BB CC'/></m>";
+
+        assert $(xml).find("[A ^= \"A\"]").size() == 2;
+        assert $(xml).find("[A ^= \"B\"]").size() == 0;
+        assert $(xml).find("[A ^= \"C\"]").size() == 0;
+    }
+
+    @Test
+    public void attributeEndWith() throws Exception {
+        String xml = "<m><e A='A B C'/><e A='AA BB CC'/></m>";
+
+        assert $(xml).find("[A $= \"A\"]").size() == 0;
+        assert $(xml).find("[A $= \"B\"]").size() == 0;
+        assert $(xml).find("[A $= \"C\"]").size() == 2;
     }
 
     @Test
     public void child() throws Exception {
-        assert $(document).find("person > person").size() == 2;
+        String xml = xml("<m><P><Q/><z><Q/></z><Q/></P><Q/></m>");
+
+        assert $(xml).find("P>Q").size() == 2;
+        assert $(xml).find("P > Q").size() == 2;
+        assert $(xml).find("P   >    Q").size() == 2;
     }
 
     @Test
-    public void next() throws Exception {
-        assert $(document).find("person + person").size() == 2;
+    public void sibling() throws Exception {
+        String xml = xml("<m><P><Q/><P/><Q/></P><Q/><Q/></m>");
+
+        assert $(xml).find("P+Q").size() == 2;
+        assert $(xml).find("P + Q").size() == 2;
+        assert $(xml).find("P   +   Q").size() == 2;
+    }
+
+    @Test
+    public void siblings() throws Exception {
+        String xml = xml("<m><P><Q/><P/><Q/></P><Q/><Q/></m>");
+
+        assert $(xml).find("P~Q").size() == 3;
+        assert $(xml).find("P ~ Q").size() == 3;
+        assert $(xml).find("P   ~   Q").size() == 3;
+    }
+
+    /**
+     * <p>
+     * Format to human-redable xml for display when assertion is fail..
+     * </p>
+     * 
+     * @param text
+     * @return
+     */
+    private static final String xml(String text) {
+        StringBuilder builder = new StringBuilder();
+
+        // format
+        I.parse(new InputSource(new StringReader(text)), new XMLWriter(builder));
+
+        return builder.toString();
     }
 }
