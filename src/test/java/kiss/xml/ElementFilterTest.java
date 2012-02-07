dd16eedcd9ae873f588df8f8b9ef73f81f72014f
Teletha <Teletha.T@gmail.com>
2012-02-07 22:12:43 +0900

    Element implements filtering API and manipulating API. Element recognizes "-" extension combinator and various extension pseuso classes.

---
+++ src/test/java/kiss/xml/ElementFilterTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss.xml;
+
+import static kiss.xml.Element.*;
+
+import org.junit.Test;
+
+/**
+ * @version 2012/02/07 19:01:50
+ */
+public class ElementFilterTest {
+
+    @Test
+    public void first() throws Exception {
+        String xml = "<m><Q class='first'/><Q/><Q class='last'/></m>";
+
+        assert $(xml).find("Q").first().attr("class").equals("first");
+    }
+
+    @Test
+    public void last() throws Exception {
+        String xml = "<m><Q class='first'/><Q/><Q class='last'/></m>";
+
+        assert $(xml).find("Q").last().attr("class").equals("last");
+    }
+}
