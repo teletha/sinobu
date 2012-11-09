a58a0a4394a2d9bb9507075b67f189f01da2db49
Teletha <Teletha.T@gmail.com>
2012-11-09 11:29:34 +0900

    Remove SAX based API.

--- src/main/java/kiss/State.java
+++ src/main/java/kiss/State.java
@@ -10,6 +10,7 @@
 package kiss;
 
 import java.io.IOException;
+import java.io.PushbackReader;
 import java.io.Reader;
 import java.nio.CharBuffer;
 
@@ -22,14 +23,14 @@ import kiss.model.PropertyWalker;
  * This is multi-purpose implementation class. Please connive this extremely-dirty code.
  * </p>
  * <ul>
- * <li>State recorder for XML serialization ({@link XMLIn})</li>
- * <li>{@link PropertyWalker} implementation for {@link I#transform(Object, Class)}</li>
+ * <li>State recorder for XML serialization</li>
  * <li>{@link Reader} implementation for {@link I#read(Readable, Object)}</li>
+ * <li>{@link PropertyWalker} implementation for {@link I#transform(Object, Class)}</li>
  * </ul>
  * 
- * @version 2011/04/01 11:21:56
+ * @version 2012/11/09 11:04:01
  */
-final class Util extends Reader implements PropertyWalker {
+final class State extends Reader implements PropertyWalker {
 
     /** The current model. */
     Model model;
@@ -37,10 +38,10 @@ final class Util extends Reader implements PropertyWalker {
     /** The curret object. */
     Object object;
 
-    /** The property for {@link XMLIn} process. */
+    /** The property for xml deserialization process. */
     Property property;
 
-    /** The current location for {@link XMLIn} process. */
+    /** The current location for deserialization process. */
     int i = 0;
 
     /** The latest cache for {@link I#read(Readable, Object)}. */
@@ -52,9 +53,44 @@ final class Util extends Reader implements PropertyWalker {
      * @param object A actual object.
      * @param model A model of the specified object.
      */
-    Util(Object object, Model model) {
+    State(Object object, Model model) {
         this.object = object;
         this.model = model;
+    }
+
+    /**
+     * <p>
+     * {@link Reader} implementation for {@link I#read(Readable, Object)}.
+     * </p>
+     * <p>
+     * Thid reader provides two functionalities. One is {@link Readable} reader and the other is
+     * {@link PushbackReader}.
+     * </p>
+     */
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException {
+        // We use ther int field as flag whether this reader shuold push back stream or not.
+        if (i == 0) {
+            // read data normally
+            return ((Readable) object).read(last = CharBuffer.wrap(cbuf, off, len));
+        } else {
+            // push back data
+            i = 0;
+
+            // Read the latest buffer with JSON header text.
+            // The sequential reading process is normal because the flag field was reset.
+            return CharBuffer.wrap(cbuf, off, len).put("a=").put((CharBuffer) last.flip()).flip().limit();
+        }
+    }
+
+    /**
+     * <p>
+     * {@link Reader} implementation for {@link I#read(Readable, Object)}.
+     * </p>
+     */
+    @Override
+    public void close() throws IOException {
+        I.quiet(object);
     }
 
     /**
@@ -65,29 +101,5 @@ final class Util extends Reader implements PropertyWalker {
 
         // never check null because PropertyWalker traverses existing properties
         this.model.set(object, dest, I.transform(node, dest.model.type));
-    }
-
-    /**
-     * @see java.io.Reader#read(char[], int, int)
-     */
-    @Override
-    public int read(char[] cbuf, int off, int len) throws IOException {
-        if (i == 0) {
-            return ((Readable) object).read(last = CharBuffer.wrap(cbuf, off, len));
-        } else {
-            // flag off
-            i = 0;
-
-            // Read the latest buffer with JSON header text.
-            return CharBuffer.wrap(cbuf, off, len).put("a=").put((CharBuffer) last.flip()).flip().limit();
-        }
-    }
-
-    /**
-     * @see java.io.Reader#close()
-     */
-    @Override
-    public void close() throws IOException {
-        I.quiet(object);
     }
 }
