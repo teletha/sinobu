3ab8b749667e4f5f92ebdc42d6073f1e71ca897e
Teletha <Teletha.T@gmail.com>
2012-11-09 09:41:07 +0900

    Revert sax based tests and classes.

---
+++ src/test/java/kiss/xml/sax/Bits.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss.xml.sax;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.AttributesImpl;
+import org.xml.sax.helpers.XMLFilterImpl;
+
+/**
+ * <p>
+ * This class is a buffer of SAX events.
+ * </p>
+ * 
+ * @version 2011/04/13 14:49:27
+ */
+public final class Bits extends XMLFilterImpl {
+
+    /** The event cache. */
+    final ArrayList<Object[]> bits = new ArrayList();
+
+    /**
+     * <p>
+     * Record character event.
+     * </p>
+     */
+    @Override
+    public void characters(char[] ch, int start, int length) {
+        bits.add(new Object[] {new String(ch, start, length)});
+    }
+
+    /**
+     * <p>
+     * Record start element event.
+     * </p>
+     */
+    @Override
+    public void startElement(String uri, String local, String name, Attributes atts) {
+        bits.add(new Object[] {uri, local, name, new AttributesImpl(atts)});
+    }
+
+    /**
+     * <p>
+     * Record end element event.
+     * </p>
+     */
+    @Override
+    public void endElement(String uri, String local, String name) {
+        bits.add(new Object[] {uri, local, name});
+    }
+}
---
+++ src/test/java/kiss/xml/sax/XMLScannerTest.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss.xml.sax;
+
+import static antibug.AntiBug.*;
+import kiss.I;
+
+import org.junit.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLFilter;
+import org.xml.sax.helpers.AttributesImpl;
+
+import antibug.xml.XML;
+
+/**
+ * @version 2012/02/14 15:44:41
+ */
+public class XMLScannerTest {
+
+    @Test
+    public void dontModify() throws Exception {
+        XML text = xml("<root/>", new XMLScanner());
+        XML expect = xml("<root/>");
+
+        assert text.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void chainFilter() throws Exception {
+        XMLFilter first = new XMLScanner();
+
+        XMLFilter second = new XMLScanner();
+        second.setParent(first);
+
+        XMLFilter third = new XMLScanner();
+        third.setParent(second);
+
+        XML text = xml("<root/>", third);
+        XML expect = xml("<root/>");
+
+        assert text.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void modifyByFilter() throws Exception {
+        XML text = xml("<root/>", new Encloser("top"));
+        XML expect = xml("<top><root/></top>");
+
+        assert text.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void modifyByChinedFilters() throws Exception {
+        XMLFilter first = new Encloser("first");
+
+        XMLFilter second = new Encloser("second");
+        second.setParent(first);
+
+        XMLFilter third = new Encloser("third");
+        third.setParent(second);
+
+        XML text = xml("<root/>", third);
+        XML expect = xml("<third><second><first><root/></first></second></third>");
+
+        assert text.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeXML() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+
+        // write xml
+        writer.startDocument();
+        writer.start("root");
+        writer.end();
+        writer.endDocument();
+
+        XML expect = xml("<root/>");
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeAttributeAndText() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+
+        // write xml
+        writer.startDocument();
+        writer.start("root", "name1", "value1", "name2", "value2");
+        writer.text("text");
+        writer.end();
+        writer.endDocument();
+
+        XML expect = xml("<root name1='value1' name2='value2'>text</root>");
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeNS() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+
+        // write xml
+        writer.startDocument();
+        writer.startPrefixMapping("", "default");
+        writer.start("root");
+        writer.end();
+        writer.endPrefixMapping("");
+        writer.endDocument();
+
+        XML expect = xml("<root xmlns='default'/>");
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeNSElement() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+
+        // write xml
+        writer.startDocument();
+        writer.startPrefixMapping("test", "first");
+        writer.start("test:root");
+        writer.startPrefixMapping("test", "second");
+        writer.start("test:in");
+        writer.end();
+        writer.endPrefixMapping("test");
+        writer.start("test:in");
+        writer.end();
+        writer.end();
+        writer.endPrefixMapping("test");
+        writer.endDocument();
+
+        XML expect = xml("<test:root xmlns:test='first'><test:in xmlns:test='second'/><test:in/></test:root>");
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeNSAttribute() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+
+        // write xml
+        writer.startDocument();
+        writer.startPrefixMapping("test", "first");
+        writer.start("root", "test:name1", "value1");
+        writer.text("text");
+        writer.end();
+        writer.endPrefixMapping("test");
+        writer.endDocument();
+
+        XML expect = xml("<root xmlns:test='first' test:name1='value1'>text</root>");
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void writeLoop() throws Exception {
+        XMLScanner writer = new XMLScanner();
+        XML doc = xml(writer);
+        XML expect = xml("<root xmlns:ns='ns'><ns:m>0</ns:m><ns:m>1</ns:m><ns:m>2</ns:m></root>");
+
+        // write xml
+        writer.startDocument();
+        writer.startPrefixMapping("ns", "ns");
+        writer.start("root");
+        for (int i = 0; i < 3; i++) {
+            writer.start("ns:m");
+            writer.text(String.valueOf(i));
+            writer.end();
+        }
+        writer.end();
+        writer.endPrefixMapping("ns");
+        writer.endDocument();
+
+        assert doc.isIdenticalTo(expect);
+    }
+
+    @Test
+    public void skipDTD() {
+        I.parse(locate("doctype.xml"), new XMLScanner());
+    }
+
+    /**
+     * Test to avoid the circular method calling.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void overrideStartElement() throws Exception {
+        XMLScanner writer = new XMLScanner() {
+
+            @Override
+            public void startElement(String uri, String localName, String name, Attributes atts) throws SAXException {
+                start("add");
+            }
+        };
+
+        // write xml
+        writer.startDocument();
+        writer.startElement("", "root", "root", new AttributesImpl());
+        writer.endElement("", "root", "root");
+        writer.endDocument();
+    }
+
+    /**
+     * Test to avoid the circular method calling.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void overrideEndElement() throws Exception {
+        XMLScanner writer = new XMLScanner() {
+
+            @Override
+            public void endElement(String uri, String localName, String name) throws SAXException {
+                start("add");
+                end();
+            }
+        };
+
+        // write xml
+        writer.startDocument();
+        writer.startElement("", "root", "root", new AttributesImpl());
+        writer.endElement("", "root", "root");
+        writer.endDocument();
+    }
+
+    /**
+     * Test to avoid the circular method calling.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void overrideCharacters() throws Exception {
+        XMLScanner writer = new XMLScanner() {
+
+            @Override
+            public void characters(char[] ch, int start, int length) throws SAXException {
+                text("@");
+            }
+        };
+
+        // write xml
+        writer.startDocument();
+        writer.startElement("", "root", "root", new AttributesImpl());
+        writer.characters("test".toCharArray(), 0, 4);
+        writer.endElement("", "root", "root");
+        writer.endDocument();
+    }
+}
