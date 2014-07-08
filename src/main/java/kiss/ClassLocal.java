a58be90b0b36f470956fe55697b21882c3b94276
Teletha <Teletha.T@gmail.com>
2014-07-09 08:46:15 +0900

    Progress...

---
+++ src/main/java/kiss/ClassLocal.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @version 2014/07/09 8:31:15
+ */
+public class ClassLocal<T> extends ClassValue<T> {
+
+    private final Map<Class, T> values = new HashMap();
+
+    private Function<Class, T> supplier;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected T computeValue(Class type) {
+        return values.compute(type, (model, prev) -> {
+            return null;
+        });
+    }
+
+    public T get(Class type, Function<Class, T> supplier) {
+        this.supplier = supplier;
+
+        return get(type);
+    }
+
+    public void set(Class type, T value) {
+
+    }
+}
