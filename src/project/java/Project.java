a198497e19231908e6f616ba7ee10bd20aae6738
Teletha <Teletha.T@gmail.com>
2015-06-17 21:42:42 +0900

    Update license.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Sinobu Development Team
+ * Copyright (C) 2015 Nameless Production Committee
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@ public class Project extends bee.api.Project {
 
     {
         product("npc", "Sinobu", "0.9.4");
+        producer("Nameless Production Committee");
         describe("Sinobu is not obsolete framework but utility, which can manipulate objects as a extremely-condensed facade.");
 
         require("npc", "antibug", "0.3").atTest();
