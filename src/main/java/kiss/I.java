b5293fcb748775d829f6e88e484a7f20dfc38ec8
Teletha <Teletha.T@gmail.com>
2015-06-25 13:56:12 +0900

    Progress...

--- src/main/java/kiss/I.java
+++ src/main/java/kiss/I.java
@@ -2646,6 +2646,10 @@ public class I implements ThreadFactory, ClassListener<Extensible> {
         modules.unload(classPath);
     }
 
+    public static void unzip(Path input, Path output) {
+
+    }
+
     public static void zip(Path input, Path output, String... patterns) {
         new Visitor(input, output, -1, null, patterns).walk().dispose();
     }
