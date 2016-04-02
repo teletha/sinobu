92aeb8edba94cbe2e7d71aab283a1d7dbf7bafbe
kangaku <kangaku@kangaku7>
2016-04-02 15:01:15 +0900

    Remove Lombok.

--- src/test/java/kiss/lambda/LambdaTest.java
+++ src/test/java/kiss/lambda/LambdaTest.java
@@ -12,39 +12,14 @@ package kiss.lambda;
 import static kiss.lambda.Lambda.*;
 
 import java.math.BigInteger;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import org.junit.Test;
 
-import kiss.Ⅲ;
-import lombok.experimental.ExtensionMethod;
-
 /**
  * @version 2014/07/20 3:33:24
  */
-@ExtensionMethod({Lambda.class})
 public class LambdaTest {
-
-    @Test
-    public void lambda() throws Exception {
-        BiFunction<String, String, String> function = (a, b) -> a + b;
-        Function<String, Function<String, String>> curried = function.curry();
-        BiFunction<String, String, String> uncurried = curried.uncurry();
-
-        System.out.println(curried.apply("test").apply("value"));
-        System.out.println(uncurried.apply("test", "value"));
-
-        Function<Ⅲ<String, String, String>, String> f = a -> a.ⅰ + a.ⅱ + a.ⅲ;
-        Function<String, Function<String, Function<String, String>>> curry3 = f.curry3();
-        System.out.println(curry3.apply("A").apply("B").apply("C"));
-
-        Function<Function<String, String>, Function<String, String>> aa = ff -> v -> {
-            return v;
-        };
-
-        Function<String, String> recursive = aa.recursive();
-    }
 
     @Test
     public void testname() throws Exception {
