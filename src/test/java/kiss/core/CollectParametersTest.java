52d4f5b5d538c2e4de4b7d1db0958c8e39b78f0f
Teletha <Teletha.T@gmail.com>
2016-05-28 08:41:00 +0900

    Add test.

--- src/test/java/kiss/core/CollectParametersTest.java
+++ src/test/java/kiss/core/CollectParametersTest.java
@@ -639,4 +639,54 @@ public class CollectParametersTest {
      */
     private static class ConsolesUI extends StackContainer<Shell, Console> {
     }
+
+    @Test
+    public void complexTypeHierarchy4() {
+        Type[] types = kiss.model.Model.collectParameters(CheckBox.class, Widget.class);
+        assert 1 == types.length;
+        assert UserStyle.class == types[0];
+    }
+
+    /**
+     * @version 2016/05/27 22:33:12
+     */
+    private static class StyleDescriptor {
+    }
+
+    /**
+     * @version 2016/05/27 22:32:52
+     */
+    private static class UserStyle extends StyleDescriptor {
+    }
+
+    /**
+     * @version 2016/05/27 22:34:05
+     */
+    private static interface Declarable {
+    }
+
+    /**
+     * @version 2016/05/27 22:33:47
+     */
+    private static abstract class Widget<Styles extends StyleDescriptor> implements Declarable {
+    }
+
+    /**
+     * @version 2016/05/27 22:33:36
+     */
+    private static abstract class LowLevelWidget<Styles extends StyleDescriptor, T extends LowLevelWidget<Styles, T>>
+            extends Widget<Styles> {
+    }
+
+    /**
+     * @version 2016/05/27 22:33:01
+     */
+    private static abstract class MarkedButton<T extends MarkedButton<T, V>, V> extends LowLevelWidget<UserStyle, T> {
+    }
+
+    /**
+     * @version 2016/05/27 22:31:56
+     */
+    private static class CheckBox<V> extends MarkedButton<CheckBox<V>, V> {
+    }
 }
