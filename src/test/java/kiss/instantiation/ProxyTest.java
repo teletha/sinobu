61d972ec2d98401b712a0d5c245db6bf5498634c
Teletha <Teletha.T@gmail.com>
2017-01-19 11:53:52 +0900

    Write proxy test.

---
+++ src/test/java/kiss/instantiation/ProxyTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss.instantiation;
+
+import org.junit.Test;
+
+import kiss.I;
+
+/**
+ * @version 2017/01/19 11:36:47
+ */
+public class ProxyTest {
+
+    @Test
+    public void proxyPublic() {
+        Public proxy = I.make(Public.class, (instance, method, args) -> "OK");
+
+        assert proxy.text().equals("OK");
+    }
+
+    @Test
+    public void proxyProtected() {
+        Protected proxy = I.make(Protected.class, (instance, method, args) -> "OK");
+
+        assert proxy.text().equals("OK");
+    }
+
+    /**
+     * @version 2017/01/19 11:37:37
+     */
+    public static interface Public {
+        String text();
+    }
+
+    /**
+     * @version 2017/01/19 11:37:37
+     */
+    static interface Protected {
+        String text();
+    }
+}
