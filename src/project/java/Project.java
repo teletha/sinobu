7cfe666b6129e0a60def2c2001c01c6d76cb2926
Teletha <Teletha.T@gmail.com>
2017-01-20 15:23:41 +0900

    Update version.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -10,7 +10,7 @@
 public class Project extends bee.api.Project {
 
     {
-        product("npc", "Sinobu", "1.0");
+        product("npc", "Sinobu", "1.0.2");
         producer("Nameless Production Committee");
         describe("Sinobu is not obsolete framework but utility, which can manipulate objects as a extremely-condensed facade.");
 
