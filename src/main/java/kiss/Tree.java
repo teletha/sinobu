b8a375cb8e404ec1d689bc5f3bb38c129b9ee1df
Teletha <Teletha.T@gmail.com>
2017-02-09 15:07:33 +0900

    Adopt murmur hash.

--- src/main/java/kiss/Tree.java
+++ src/main/java/kiss/Tree.java
@@ -528,23 +528,70 @@ public abstract class Tree<N extends Consumer<N>> {
         };
     }
 
-    /** The bit mask size. */
-    private static final int[] MASK = {0, 8, 16, 24};
+    // Constants for 32 bit variant
+    private static final int C1_32 = 0xcc9e2d51;
+
+    private static final int C2_32 = 0x1b873593;
+
+    private static final int R1_32 = 15;
+
+    private static final int R2_32 = 13;
+
+    private static final int M_32 = 5;
+
+    private static final int N_32 = 0xe6546b64;
 
     /**
-     * <p>
-     * FNV-1a 32bit hash function.
-     * </p>
-     * 
-     * @param value A target value.
+     * Murmur3 32-bit hash function.
+     *
+     * @param value A input value.
      * @return A hash value.
      */
-    private final int hash(int value) {
-        int hash = 0x811c9dc5;
+    public static int hash(int value) {
+        byte[] data = new byte[] {(byte) (value >> 24), (byte) (value >> 16), (byte) (value >> 8), (byte) value};
 
-        for (int i = 0; i < 4; i++) {
-            hash = (hash ^ (0x000000ff & (value >>> MASK[i]))) * 0x01000193;
+        int hash = 0;
+        final int nblocks = 4 >> 2;
+
+        // body
+        for (int i = 0; i < nblocks; i++) {
+            int i_4 = i << 2;
+            int k = (data[i_4] & 0xff) | ((data[i_4 + 1] & 0xff) << 8) | ((data[i_4 + 2] & 0xff) << 16) | ((data[i_4 + 3] & 0xff) << 24);
+
+            // mix functions
+            k *= C1_32;
+            k = Integer.rotateLeft(k, R1_32);
+            k *= C2_32;
+            hash ^= k;
+            hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;
         }
+
+        // tail
+        int idx = nblocks << 2;
+        int k1 = 0;
+        switch (4 - idx) {
+        case 3:
+            k1 ^= data[idx + 2] << 16;
+        case 2:
+            k1 ^= data[idx + 1] << 8;
+        case 1:
+            k1 ^= data[idx];
+
+            // mix functions
+            k1 *= C1_32;
+            k1 = Integer.rotateLeft(k1, R1_32);
+            k1 *= C2_32;
+            hash ^= k1;
+        }
+
+        // finalization
+        hash ^= 4;
+        hash ^= (hash >>> 16);
+        hash *= 0x85ebca6b;
+        hash ^= (hash >>> 13);
+        hash *= 0xc2b2ae35;
+        hash ^= (hash >>> 16);
+
         return hash;
     }
 }
