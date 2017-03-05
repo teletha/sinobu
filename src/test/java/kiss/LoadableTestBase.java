b54cd8dce684833b93371bfd89abb8f63281329f
Teletha <Teletha.T@gmail.com>
2017-03-05 22:29:25 +0900

    Update license.

--- src/test/java/kiss/LoadableTestBase.java
+++ src/test/java/kiss/LoadableTestBase.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *          http://opensource.org/licenses/mit-license.php
+ *          https://opensource.org/licenses/MIT
  */
 package kiss;
 
