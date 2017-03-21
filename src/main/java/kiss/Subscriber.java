1b7bdd4d1752bb955e2647fc7cc05d46d89b5ed9
Teletha <Teletha.T@gmail.com>
2017-03-21 15:07:17 +0900

    ?

---
+++ src/main/java/kiss/Subscriber.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2017 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package kiss;
+
+/**
+ * @version 2017/03/21 15:06:42
+ */
+public class Subscriber {
+
+}
