f22b15506f31c25ddc5520eacd196304202fe7d7
Teletha <Teletha.T@gmail.com>
2017-12-26 09:59:29 +0900

    Custom codec of enum has high priority.

--- src/test/java/kiss/codec/EnumTest.java
+++ src/test/java/kiss/codec/EnumTest.java
@@ -14,11 +14,12 @@ import org.junit.Test;
 import kiss.Decoder;
 import kiss.Encoder;
 import kiss.I;
+import kiss.LoadableTestBase;
 
 /**
- * @version 2015/10/20 17:47:06
+ * @version 2017/12/26 9:59:02
  */
-public class EnumTest {
+public class EnumTest extends LoadableTestBase {
 
     @Test
     public void codec() throws Exception {
@@ -26,6 +27,18 @@ public class EnumTest {
         Encoder<OverrideToString> encoder = I.find(Encoder.class, OverrideToString.class);
         assert decoder.decode("A") == OverrideToString.A;
         assert encoder.encode(OverrideToString.A).equals("A");
+    }
+
+    @Test
+    public void custom() throws Exception {
+        loadClasses();
+
+        Decoder<Custom> decoder = I.find(Decoder.class, Custom.class);
+        Encoder<Custom> encoder = I.find(Encoder.class, Custom.class);
+        assert decoder.decode("OK") == Custom.OK;
+        assert decoder.decode("Ok") == Custom.OK;
+        assert decoder.decode("ok") == Custom.OK;
+        assert encoder.encode(Custom.OK).equals("ok");
     }
 
     /**
@@ -50,6 +63,45 @@ public class EnumTest {
         @Override
         public String toString() {
             return name;
+        }
+    }
+
+    /**
+     * @version 2017/12/26 9:54:55
+     */
+    private static enum Custom {
+        OK, NG;
+    }
+
+    /**
+     * @version 2017/12/26 9:55:31
+     */
+    @SuppressWarnings("unused")
+    private static class CustomCodec implements Decoder<Custom>, Encoder<Custom> {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String encode(Custom value) {
+            return value.toString().toLowerCase();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Custom decode(String value) {
+            switch (value.toLowerCase()) {
+            case "ok":
+                return Custom.OK;
+
+            case "ng":
+                return Custom.NG;
+            }
+            // If this exception will be thrown, it is bug of this program. So we must rethrow the
+            // wrapped error in here.
+            throw new Error();
         }
     }
 }
