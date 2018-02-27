c240445ce1768e775de7cde5d99149fa78a47c99
Teletha <Teletha.T@gmail.com>
2018-02-27 13:36:26 +0900

    Update license.

--- src/test/java/kiss/signal/TimeoutTest.java
+++ src/test/java/kiss/signal/TimeoutTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2017 Nameless Production Committee
+ * Copyright (C) 2018 Nameless Production Committee
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *          http://opensource.org/licenses/mit-license.php
+ *          https://opensource.org/licenses/MIT
  */
 package kiss.signal;
 
@@ -14,7 +14,7 @@ import org.junit.Test;
 import kiss.SignalTester;
 
 /**
- * @version 2017/04/06 15:12:36
+ * @version 2018/02/27 13:35:47
  */
 public class TimeoutTest extends SignalTester {
 
