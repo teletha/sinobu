0a7b20ab33f245c0a652f0aae5a6314670f20f02
Teletha <Teletha.T@gmail.com>
2018-04-02 16:25:06 +0900

    Remove unused class.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -15,7 +15,7 @@ public class Project extends bee.api.Project {
         describe("Sinobu is not obsolete framework but utility, which can manipulate objects as a extremely-condensed facade.");
 
         require("com.github.teletha", "antibug", "0.3").atTest();
-        require("io.reactivex.rxjava2", "rxjava", "2.1.6").atTest();
+        require("io.reactivex.rxjava2", "rxjava", "[2.0.0,)").atTest();
 
         versionControlSystem("https://github.com/teletha/Sinobu");
     }
