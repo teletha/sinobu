dde4b3bd64dbbe7e76f01fa76339080b25726869
Teletha <Teletha.T@gmail.com>
2019-07-30 16:11:28 +0900

    Change error handling.

--- src/main/java/kiss/Observer.java
+++ src/main/java/kiss/Observer.java
@@ -55,6 +55,8 @@ public interface Observer<V> extends Consumer<V> {
         UncaughtExceptionHandler handler = Thread.getDefaultUncaughtExceptionHandler();
         if (handler != null) {
             handler.uncaughtException(Thread.currentThread(), e);
+        } else {
+            e.printStackTrace();
         }
         throw I.quiet(e);
     }
