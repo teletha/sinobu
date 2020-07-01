1e269eb45dcd4f140fc4d10090863a228c4e5ff5
Teletha <Teletha.T@gmail.com>
2020-07-01 11:55:15 +0900

    Rewrite license.

--- src/main/java/kiss/Signal.java
+++ src/main/java/kiss/Signal.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2019 Nameless Production Committee
+ * Copyright (C) 2020 Nameless Production Committee
  *
  * Licensed under the MIT License (the "License");
- * you may not use this file except in compliance preassign the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *          https://opensource.org/licenses/MIT
