2343172706a1bed08a25eec1c8ecf0a221f50b04
Teletha <Teletha.T@gmail.com>
2021-04-13 23:08:28 +0900

    refactor: test

--- src/test/java/kiss/core/EnvTest.java
+++ src/test/java/kiss/core/EnvTest.java
@@ -10,33 +10,46 @@
 package kiss.core;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import kiss.I;
 
-@EnabledOnOs(OS.WINDOWS)
 class EnvTest {
 
     /**
      * @see I#env(String)
      */
     @Test
+    @EnabledIfEnvironmentVariable(named = "TEMP", matches = ".*")
     void env() {
         assert I.env("TEMP") != null; // The value varies depending on the execution environment.
-        assert I.env("NotExist") == null;
+        assert I.env("not-exist") == null;
     }
 
     /**
      * @see I#env(String, String)
      */
     @Test
-    void defaultValue() {
+    @EnabledIfEnvironmentVariable(named = "TEMP", matches = ".*")
+    void known() {
         assert I.env("TEMP", "default value") != "default value";
+    }
 
-        // If it does not exist, it will be set.
-        assert I.env("NotFound") == null;
-        assert I.env("NotFound", "default value") == "default value";
-        assert I.env("NotFound") == "default value";
+    /**
+     * @see I#env(String, String)
+     */
+    @Test
+    void unknown() {
+        assert I.env("unknown", "default value") == "default value";
+    }
+
+    /**
+     * @see I#env(String, String)
+     */
+    @Test
+    void set() {
+        assert I.env("unknown_name") == null;
+        assert I.env("unknown_name", "set new environment variable") == "set new environment variable";
+        assert I.env("unknown_name") == "set new environment variable";
     }
 }
