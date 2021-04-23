75eeb42ee6c30a4ddf4c9f6298309ed57bd9198d
Teletha <Teletha.T@gmail.com>
2021-04-23 17:41:31 +0900

    progress...

---
+++ src/test/java/doc/IndexDoc.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package doc;
+
+/**
+ * <h2>Introduction</h2>
+ * <p>
+ * This library aims to simplify and highly condense the functions related to domains that are
+ * frequently encountered in real-world development projects, making them easier to use. Some
+ * specific domains are listed below.
+ * </p>
+ * <ul>
+ * <li><a href="https://en.wikipedia.org/wiki/Dependency_injection">Dependency Injection</a></li>
+ * <li>Object lifecycle management</li>
+ * <li><a href="https://en.wikipedia.org/wiki/JavaBeans">JavaBeans</a>-like property based type
+ * modeling</li>
+ * <li>HTTP(S)</li>
+ * <li><a href="https://en.wikipedia.org/wiki/JSON">JSON</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/HTML">HTML</a>(<a href=
+ * "https://en.wikipedia.org/wiki/XML">XML</a>)</li>
+ * <li>Reactive Programming (<a href="http://reactivex.io/">Rx</a>)</li>
+ * <li>Asynchronous processing</li>
+ * <li>Parallel processing</li>
+ * <li>Multilingualization</li>
+ * <li>Template engine(<a href="https://mustache.github.io/mustache.5.html">Mustache</a>)</li>
+ * <li>Dynamic plug-in mechanisms</li>
+ * <li>Domain specific languages</li>
+ * <li>Persistence</li>
+ * </ul>
+ * <p>
+ * With a few exceptions, Sinobu and its APIs are designed to be simple to use and easy to
+ * understand by adhering to the following principles.
+ * </p>
+ * <ul>
+ * <li><a href="https://en.wikipedia.org/wiki/KISS_principle">Keep it stupid simple</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Less_is_more_(architecture)">Less is more</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Type_safety">Type safety</a></li>
+ * <li>Refactoring safety</li>
+ * </ul>
+ */
+public class IndexDoc {
+
+    /**
+     * 
+     */
+    Void Install;
+
+    /**
+     * <h2>Dependency Injection</h2>
+     * <p>
+     * </p>
+     */
+    public class DipendencyInjectionDoc {
+    }
+}
