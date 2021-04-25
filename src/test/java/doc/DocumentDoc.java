ad88369a0c7f2ec1d27f6b715aca823c667c9417
Teletha <Teletha.T@gmail.com>
2021-04-25 09:14:50 +0900

    progress...

---
+++ src/test/java/doc/DocumentDoc.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2021 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package doc;
+
+/**
+ * <h2>Document</h2>
+ */
+public class DocumentDoc {
+
+    /**
+     * <p>
+     * This library aims to simplify and highly condense the functions related to domains that are
+     * frequently encountered in real-world development projects, making them easier to use. Some
+     * specific domains are listed below.
+     * </p>
+     * <ul>
+     * <li><a href="https://en.wikipedia.org/wiki/Dependency_injection">Dependency
+     * Injection</a></li>
+     * <li>Object lifecycle management</li>
+     * <li><a href="https://en.wikipedia.org/wiki/JavaBeans">JavaBeans</a>-like property based type
+     * modeling</li>
+     * <li>HTTP(S)</li>
+     * <li><a href="https://en.wikipedia.org/wiki/JSON">JSON</a></li>
+     * <li><a href="https://en.wikipedia.org/wiki/HTML">HTML</a>(<a href=
+     * "https://en.wikipedia.org/wiki/XML">XML</a>)</li>
+     * <li>Reactive Programming (<a href="http://reactivex.io/">Rx</a>)</li>
+     * <li>Asynchronous processing</li>
+     * <li>Parallel processing</li>
+     * <li>Multilingualization</li>
+     * <li>Template engine(<a href="https://mustache.github.io/mustache.5.html">Mustache</a>)</li>
+     * <li>Dynamic plug-in mechanisms</li>
+     * <li>Domain specific languages</li>
+     * <li>Persistence</li>
+     * </ul>
+     * <p>
+     * With a few exceptions, Sinobu and its APIs are designed to be simple to use and easy to
+     * understand by adhering to the following principles.
+     * </p>
+     * <ul>
+     * <li><a href="https://en.wikipedia.org/wiki/KISS_principle">Keep it stupid simple</a></li>
+     * <li><a href="https://en.wikipedia.org/wiki/Less_is_more_(architecture)">Less is more</a></li>
+     * <li><a href="https://en.wikipedia.org/wiki/Type_safety">Type safety</a></li>
+     * <li>Refactoring safety</li>
+     * </ul>
+     */
+    public DocumentDoc Introduction;
+
+    /**
+     * <p>
+     * It is probably easiest to use a build tool such as
+     * <a href="https://maven.apache.org/">Maven</a> or <a href="https://gradle.org/">Gradle</a>.
+     * </p>
+     */
+    public DocumentDoc How_to_install;
+
+    class Managing_object_lifestyle {
+        /**
+         * <h2>What exactly is a lifestyle?</h2>
+         * <p>
+         * モックアップとは、画面レイアウト情報をもとに、HTML形式で画面イメージを出力する機能です。通常、設計がおわった段階では設計書をもとにレビューを行いますが、この段階では動くモノ
+         * POO（アプリケーション）はありません。ですので、「コントロールがない」「桁数が異なる」といった設計ミスは見つけることができても、COOL
+         * 「画面遷移の自然さ」や、「データ入力の効率」などの使い勝手の部分までチェックすることは非常に難しいと思います。モックアップはHTML形式でアプリケーションのProtptype
+         * を出力する機能ですので、設計段階でこのような使い勝手も含めてレビューが可能となります。なお、入力は既存の設計データ（画面レイアウト情報）を使用しますので、追加のデータ入力は必要ありません。
+         * </p>
+         */
+        public DocumentDoc What_exactly_is_a_lifestyle;
+
+        /**
+         * <p>
+         * SpringのDIコンテナ（≒IoCコンテナ）が起動すると、JavaConfig またはXMLのBean定義に基づいて、Spring Beanがインスタンス化されます。
+         * インスタンス化はフレームワークが自動で行ってくれて、最終的にJava Beanが「使用可能な状態」になります。 この「使用可能な状態」になったJava
+         * BeanをDIコンテナが管理してくれて、プログラマが書いたコードに注入（DI）してくれるわけです。
+         * </p>
+         */
+        public DocumentDoc Use_a_customized_Lifestyle;
+    }
+
+    class Dipendency_Injection {
+        public DocumentDoc Injection_Type;
+
+        public DocumentDoc Circular_Reference;
+    }
+
+    class HTTP {
+        public DocumentDoc Request_and_Response;
+
+        public DocumentDoc Supported_Type;
+
+        public DocumentDoc Cookie;
+
+        public DocumentDoc Authentication;
+    }
+
+    class JSON {
+
+        public DocumentDoc Parsing;
+
+        public DocumentDoc Writing;
+
+        public DocumentDoc Mapping_to_Model;
+
+        public DocumentDoc Mapping_from_Model;
+    }
+
+    class HTML {
+        public DocumentDoc Parsing;
+
+        public DocumentDoc Writing;
+
+        public DocumentDoc Support_CSS_Selector;
+
+        public DocumentDoc Manipulation;
+
+        public DocumentDoc Traverse;
+    }
+
+    class ReactiveX {
+
+        public DocumentDoc Signal;
+
+        public DocumentDoc Subscribe;
+
+        public DocumentDoc Unsubscribe;
+
+        public DocumentDoc Operators;
+    }
+
+    class Template_Engine {
+        public DocumentDoc Variable;
+
+        public DocumentDoc Section;
+
+        public DocumentDoc Comment;
+    }
+
+    class Plugin {
+
+        public DocumentDoc Extension_Point;
+
+        public DocumentDoc Extension;
+
+        public DocumentDoc Dynamic_Loading;
+
+        public DocumentDoc Query_Extension;
+    }
+
+    class DSL {
+        public DocumentDoc Tree_Structured_Data;
+
+        public DocumentDoc Node_and_Leaf;
+
+        public DocumentDoc HTML_Sample;
+    }
+
+    class Persistence {
+        public DocumentDoc Save_Data;
+
+        public DocumentDoc Save_Automatically;
+
+        public DocumentDoc Config_Location;
+    }
+}
