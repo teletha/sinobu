a66b1f332d501d43543371c120958ffc2257f2c7
Teletha <Teletha.T@gmail.com>
2021-10-08 10:05:00 +0900

    fix: Singleton accepts the upper-bounded implementation class.

--- src/main/java/kiss/Singleton.java
+++ src/main/java/kiss/Singleton.java
@@ -25,7 +25,7 @@ public class Singleton<M> implements Lifestyle<M> {
      * 
      * @param modelClass A target class.
      */
-    protected Singleton(Class<M> modelClass) {
+    protected Singleton(Class<? extends M> modelClass) {
         instance = I.prototype(modelClass).get();
     }
 
