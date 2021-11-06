333c5c0b33dc9bd66d8eeee0b4d35518e1a939fa
Teletha <Teletha@users.noreply.github.com>
2021-11-06 16:21:52 +0000

    deploy: 7acd8a91ffb1ae8aa663854b229acca5a3e4ae64

---
+++ root.js
@@ -0,0 +1,519 @@
+const root = {
+	"docs": [
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Introduction.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Introduction.html#doc.DocumentDoc.Introduction.Purpose_of_use",
+					"subs": [],
+					"title": "Purpose of use"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Introduction.html#doc.DocumentDoc.Introduction.How_to_install",
+					"subs": [],
+					"title": "How to install"
+				}
+			],
+			"title": "Introduction"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.What_do_you_mean_by_lifestyle_",
+					"subs": [
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.What_do_you_mean_by_lifestyle_.Creating_an_object",
+							"subs": [],
+							"title": "Creating an object"
+						}
+					],
+					"title": "What do you mean by lifestyle?"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.Defining_lifestyle",
+					"subs": [
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.Defining_lifestyle.Pre_defined_lifestyle",
+							"subs": [],
+							"title": "Pre defined lifestyle"
+						},
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.Defining_lifestyle.Custom_lifestyle",
+							"subs": [],
+							"title": "Custom lifestyle"
+						},
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.Defining_lifestyle.Builtin_types",
+							"subs": [],
+							"title": "Builtin types"
+						}
+					],
+					"title": "Defining lifestyle"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Managing_object_lifestyle.html#doc.DocumentDoc.Managing_object_lifestyle.Registering_lifestyle",
+					"subs": [],
+					"title": "Registering lifestyle"
+				}
+			],
+			"title": "Managing object lifestyle"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Dependency_Injection.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Dependency_Injection.html#doc.DocumentDoc.Dependency_Injection.The_need_for_Dependency_Injection",
+					"subs": [],
+					"title": "The need for Dependency Injection"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Dependency_Injection.html#doc.DocumentDoc.Dependency_Injection.Circular_Reference",
+					"subs": [],
+					"title": "Circular Reference"
+				}
+			],
+			"title": "Dependency Injection"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.HTTP.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTTP.html#doc.DocumentDoc.HTTP.Request_and_Response",
+					"subs": [],
+					"title": "Request and Response"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTTP.html#doc.DocumentDoc.HTTP.Supported_Type",
+					"subs": [],
+					"title": "Supported Type"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTTP.html#doc.DocumentDoc.HTTP.Cookie",
+					"subs": [],
+					"title": "Cookie"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTTP.html#doc.DocumentDoc.HTTP.Authentication",
+					"subs": [],
+					"title": "Authentication"
+				}
+			],
+			"title": "HTTP"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.JSON.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Reading_JSON",
+					"subs": [
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Reading_JSON.Access_to_the_value",
+							"subs": [],
+							"title": "Access to the value"
+						},
+						{
+							"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Reading_JSON.Access_to_the_nested_value",
+							"subs": [],
+							"title": "Access to the nested value"
+						}
+					],
+					"title": "Reading JSON"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Writing_JSON",
+					"subs": [],
+					"title": "Writing JSON"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Mapping_to_Model",
+					"subs": [],
+					"title": "Mapping to Model"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.JSON.html#doc.DocumentDoc.JSON.Mapping_from_Model",
+					"subs": [],
+					"title": "Mapping from Model"
+				}
+			],
+			"title": "JSON"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.HTML.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTML.html#doc.DocumentDoc.HTML.Parsing",
+					"subs": [],
+					"title": "Parsing"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTML.html#doc.DocumentDoc.HTML.Writing",
+					"subs": [],
+					"title": "Writing"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTML.html#doc.DocumentDoc.HTML.Support_CSS_Selector",
+					"subs": [],
+					"title": "Support CSS Selector"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTML.html#doc.DocumentDoc.HTML.Manipulation",
+					"subs": [],
+					"title": "Manipulation"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.HTML.html#doc.DocumentDoc.HTML.Traverse",
+					"subs": [],
+					"title": "Traverse"
+				}
+			],
+			"title": "HTML"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.ReactiveX.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.ReactiveX.html#doc.DocumentDoc.ReactiveX.Signal",
+					"subs": [],
+					"title": "Signal"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.ReactiveX.html#doc.DocumentDoc.ReactiveX.Subscribe",
+					"subs": [],
+					"title": "Subscribe"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.ReactiveX.html#doc.DocumentDoc.ReactiveX.Unsubscribe",
+					"subs": [],
+					"title": "Unsubscribe"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.ReactiveX.html#doc.DocumentDoc.ReactiveX.Operators",
+					"subs": [],
+					"title": "Operators"
+				}
+			],
+			"title": "ReactiveX"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Template_Engine.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Template_Engine.html#doc.DocumentDoc.Template_Engine.Variable",
+					"subs": [],
+					"title": "Variable"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Template_Engine.html#doc.DocumentDoc.Template_Engine.Section",
+					"subs": [],
+					"title": "Section"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Template_Engine.html#doc.DocumentDoc.Template_Engine.Comment",
+					"subs": [],
+					"title": "Comment"
+				}
+			],
+			"title": "Template Engine"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Plugin.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Plugin.html#doc.DocumentDoc.Plugin.Extension_Point",
+					"subs": [],
+					"title": "Extension Point"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Plugin.html#doc.DocumentDoc.Plugin.Extension",
+					"subs": [],
+					"title": "Extension"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Plugin.html#doc.DocumentDoc.Plugin.Dynamic_Loading",
+					"subs": [],
+					"title": "Dynamic Loading"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Plugin.html#doc.DocumentDoc.Plugin.Query_Extension",
+					"subs": [],
+					"title": "Query Extension"
+				}
+			],
+			"title": "Plugin"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.DSL.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.DSL.html#doc.DocumentDoc.DSL.Tree_Structured_Data",
+					"subs": [],
+					"title": "Tree Structured Data"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.DSL.html#doc.DocumentDoc.DSL.Node_and_Leaf",
+					"subs": [],
+					"title": "Node and Leaf"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.DSL.html#doc.DocumentDoc.DSL.HTML_Sample",
+					"subs": [],
+					"title": "HTML Sample"
+				}
+			],
+			"title": "DSL"
+		},
+		{
+			"path": "/sinobu/doc/doc.DocumentDoc.Persistence.html",
+			"subs": [
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Persistence.html#doc.DocumentDoc.Persistence.Save_Data",
+					"subs": [],
+					"title": "Save Data"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Persistence.html#doc.DocumentDoc.Persistence.Save_Automatically",
+					"subs": [],
+					"title": "Save Automatically"
+				},
+				{
+					"path": "/sinobu/doc/doc.DocumentDoc.Persistence.html#doc.DocumentDoc.Persistence.Config_Location",
+					"subs": [],
+					"title": "Config Location"
+				}
+			],
+			"title": "Persistence"
+		}
+	],
+	"modules": [],
+	"packages": [
+		"kiss",
+		"kiss.model"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Decoder",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Disposable",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Encoder",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Extensible",
+			"packageName": "kiss",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ExtensionFactory",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "I",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JSON",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Lifestyle",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Managed",
+			"packageName": "kiss",
+			"type": "Annotation"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Model",
+			"packageName": "kiss.model",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Narrow",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Observer",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Property",
+			"packageName": "kiss.model",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Signal",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Signaling",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Singleton",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Storable",
+			"packageName": "kiss",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Tree",
+			"packageName": "kiss",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TreeNode",
+			"packageName": "kiss",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Variable",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Wise",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseBiConsumer",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseBiFunction",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseConsumer",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseFunction",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseRunnable",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseSupplier",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseTriConsumer",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WiseTriFunction",
+			"packageName": "kiss",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "XML",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Ⅱ",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Ⅲ",
+			"packageName": "kiss",
+			"type": "Class"
+		}
+	]
+}
