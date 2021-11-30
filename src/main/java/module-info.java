b335dd99d1a2877e1099984c8d082b7feb8f0cc3
Teletha <Teletha.T@gmail.com>
2021-11-30 13:38:30 +0900

    chore: add module-info.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module sinobu {
+    requires java.net.http;
+
+    requires java.xml;
+
+    requires static java.logging;
+
+    requires static java.compiler;
+
+    exports kiss;
+
+    exports kiss.model;
+}
