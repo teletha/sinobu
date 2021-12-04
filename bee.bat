5378d8479b93f8d44806ebd060b7d17cb9a5b570
Teletha <Teletha.T@gmail.com>
2021-12-04 20:24:20 +0900

    remode wrapper commnad

--- bee.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET bee="%JAVA_HOME%/lib/bee/bee-0.13.0.jar"
-if not exist %bee% (
-  echo %bee% is not found, try to download it from network.
-  curl -#L -o %bee% https://jitpack.io/com/github/teletha/bee/0.13.0/bee-0.13.0.jar
-)
-java -javaagent:%bee% -cp %bee% bee.Bee %*
