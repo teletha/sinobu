376e2149cfbcd32625703e066bbbfe569f0071fc
Teletha <Teletha.T@gmail.com>
2022-09-13 18:26:52 +0900

    progress

---
+++ src/test/java/kiss/model/ReflectionGetterBenchmark.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 The SINOBU Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package kiss.model;
+
+import java.lang.reflect.Method;
+
+import antibug.profiler.Benchmark;
+import kiss.I;
+import kiss.WiseFunction;
+
+public class ReflectionGetterBenchmark {
+
+    public static void main(String[] args) throws Throwable {
+        Benchmark benchmark = new Benchmark().novisualize();
+        ReflectionGetterBenchmark base = new ReflectionGetterBenchmark();
+
+        Method method = ReflectionGetterBenchmark.class.getMethod("one");
+        benchmark.measure("Reflection", () -> {
+            try {
+                return method.invoke(base);
+            } catch (Exception e) {
+                throw I.quiet(e);
+            }
+        });
+
+        WiseFunction function = Model.createGetter(method);
+        benchmark.measure("LambdaMetaFactory", () -> {
+            try {
+                return function.apply(base);
+            } catch (Throwable e) {
+                throw I.quiet(e);
+            }
+        });
+
+        benchmark.measure("DirectCall", () -> {
+            try {
+                return base.one();
+            } catch (Throwable e) {
+                throw I.quiet(e);
+            }
+        });
+
+        benchmark.perform();
+    }
+
+    public String one() {
+        return "text";
+    }
+}
