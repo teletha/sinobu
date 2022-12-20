9a566debfcca9aa6746fb034829e0922dc294592
Teletha <Teletha.T@gmail.com>
2022-12-20 16:26:11 +0900

    remove unused import

--- src/test/java/kiss/LogBenchmark.java
+++ src/test/java/kiss/LogBenchmark.java
@@ -38,7 +38,6 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.FileAppender;
-import kiss.LogBenchmark.ModifiableFormatter;
 
 public class LogBenchmark {
 
