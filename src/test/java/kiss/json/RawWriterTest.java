0c45f157d4dd08216222a1a2a4d3db057b1227d7
Teletha <Teletha.T@gmail.com>
2025-05-14 12:17:27 +0900

    remove debug code

--- src/test/java/kiss/json/RawWriterTest.java
+++ src/test/java/kiss/json/RawWriterTest.java
@@ -25,7 +25,6 @@ class RawWriterTest {
         String encoded = original.toString();
         JSON decoded = I.json(encoded);
         assert encoded.equals(decoded.toString());
-        System.out.println(encoded);
 
         for (Consumer<String> validator : validators) {
             validator.accept(encoded);
