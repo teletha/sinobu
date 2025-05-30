d3268dba9bcd09b10b90aff8f49ef906cd9f3104
teletha <teletha@users.noreply.github.com>
2025-05-30 16:46:18 +0000

    deploy: 78b4650d8ea09c2b53ee8930b32c8f5551237c6c

--- root.js
+++ root.js
@@ -43,13 +43,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "HolderGenerator2",
-			"packageName": "kiss",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "I",
 			"packageName": "kiss",
 			"type": "Class"
